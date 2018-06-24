d481f1a459eb2d5aaf0eeca21a3b813a367f8096
louis <smallbanglouis@gmail.com>
2018-06-24 22:17:09 +0800

    finished dataset.py

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 import os
 
+import cv2
 from torch.utils.data import Dataset
 
 def create_datasets(dataset_dir, skip_invalid=True, skip_occlusion=True,
@@ -37,28 +38,41 @@ def create_datasets(dataset_dir, skip_invalid=True, skip_occlusion=True,
                     coordinate = (int(bbox[1]), int(bbox[0]), int(bbox[1])+int(bbox[3]), int(bbox[0])+int(bbox[2]))
                     coordinates.append(coordinate)
 
-                processed_annotation.append({
-                    'image_path': file_path,
-                    'coordinates': coordinates
-                })
+                processed_annotation.append((
+                    file_path,
+                    coordinates
+                ))
                 cursor = cursor + 2 + face_count
 
+    train_dataset = FDDBDataset(os.path.join(dataset_dir, 'WIDER_train/images'), train_processed_annotation)
+    validation_dataset = FDDBDataset(os.path.join(dataset_dir, 'WIDER_val/images'), val_processed_annotation)
 
-    return val_processed_annotation, train_processed_annotation
+    return train_dataset, validation_dataset
 
 
 class FDDBDataset():
 
-    def __init__(self, fddb_annotation_dir, fddb_dataset_dir):
+    def __init__(self, images_dir, annotation, transform=None):
         super().__init__()
-        self.fddb_annotation_dir = fddb_annotation_dir
-        self.fddb_dataset_dir = fddb_dataset_dir
+        self.images_dir = images_dir
+        self.annotation = annotation
+        self.transform = transform
+
+    def __image_loader(self, image_path):
+        return cv2.imread(image_path)
 
     def __len__(self):
-        return 10
+        return len(self.annotation)
 
     def __getitem__(self, index):
-        return 10
+        file_path, coordinates = self.annotation[index]
+        file_path = os.path.join(self.images_dir, file_path)
+        image = self.__image_loader(file_path)
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, coordinates
 
 
 if __name__ == "__main__":
